9f770267d501ad96c257da4faed4cd7df811d4f1
shivamgohri <shivamgohri93@gmail.com>
2020-04-25 20:00:39 +0530

    all html added, added navbar

---
+++ search.js
@@ -0,0 +1,47 @@
+var searchQuery;
+
+$(document).ready(function(){
+    $("#search").on("keyup", function() {
+        
+        var value = $(this).val().toLowerCase();
+      
+        if(value == "guides" || value == "guide" || value == "help"){
+            searchQuery = "guides";
+        }
+        else if(value == "logs" || value == "log"){
+            searchQuery = "logs";
+        }
+        else if(value == "live data" || value == "data" || value == "live"){
+            searchQuery = "live data";
+        }
+        else if(value == "home" || value == "model" || value == "predict"){
+            searchQuery = "home";
+        }
+        else if(value == "log in" || value == "login" || value == "signin" || value == "sign in"){
+            searchQuery = "log in";
+        }
+
+
+    });
+
+  });
+
+function updateSearch(){   
+    
+    if(searchQuery == "guides"){
+        location.href="guides.html";
+    }
+    else if(searchQuery == "logs"){
+        location.href="logs.html";
+    }
+    else if(searchQuery == "live data"){
+        location.href="live.html";
+    }
+    else if(searchQuery == "home"){
+        location.href="home.html";
+    }
+    else if(searchQuery == "log in"){
+        location.href="login.html";
+    }
+
+}
