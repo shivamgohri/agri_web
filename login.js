3653aac42c2219f6c4343b20dd118dedcb1e8486
shivamgohri <shivamgohri93@gmail.com>
2020-05-11 19:47:50 +0530

    firebase login added

--- login.js
+++ login.js
@@ -1,63 +1,157 @@
-var userName;
-var farmer_id;
-var key;
-var nameData;
-var idData;
+var userEmail;
+var userPassword;
 
-$(document).ready(function() {
-  check();
-})
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    userEmail = user.email;
 
-function check(){
+    document.getElementById('logDIV').innerHTML = "Hi, " + userEmail + "!";
+    document.getElementById('login_text').innerHTML = "You're logged in as : " + "<b>" + userEmail + "</b>" ;
 
-  var nameRequest = $.getJSON("https://smart-agriculture-deloitte.firebaseio.com/Users.json", function(json){
-      nameData = json;
-  });
-
-  var idRequest = $.getJSON("https://smart-agriculture-deloitte.firebaseio.com/FarmerID.json", function(json){
-      idData = json;
-  });
-
-  $.when(nameRequest, idRequest).then(function() {
-    userName = nameData[nameData.length-1]
-    document.getElementById('logDIV').innerHTML = "Hi, " + userName + "!";
-  });
-
-}
-
-
-function push(){
-  if( username==undefined || farmer_id==undefined ){
-    alert("Type details!");
+  } else {
+    // No user is signed in.
+    document.getElementById('logDIV').innerHTML = "Log-In";
   }
-  else{
-    userName = $("#username").val();
-    farmer_id = $("#farmerid").val();
-
-    // firebase.database().ref('Users').push( userName );
-    // firebase.database().ref('FarmerID').push( farmer_id );
-
-    $("#username").val('');
-    $("#farmerid").val('');
-    $("#password").val('');
-
-    check();
-    location.href = "home.html";
-  }
-}
-
-$("#username").change(function() {
-  userName = $("#username").val();
-});
-
-$("#farmerid").change(function() {
-  farmer_id = $("#farmerid").val();
 });
 
 
 $("#loginButton").on("click", function(evt) {
   evt.preventDefault();
 
-  push();
+  var email = $("#email_field").val();
+  var password = $("#password_field").val();
 
+  firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    document.getElementById('login_status').innerHTML = "<font color='red'>" + errorMessage + "</font>" ;
+    // ...
+  });
 });
+
+$("#logoutButton").on("click", function(evt) {
+  firebase.auth().signOut();
+});
+
+
+function showsignup(){
+  document.getElementById('signup_div').style.display = "flex";
+  document.getElementById('login_div').style.display = "none";
+  document.getElementById('wrapper_div').style.display = "none";
+  document.getElementById('forgot_id').style.display = "none";
+  document.getElementById('logDIV').innerHTML = "Sign-Up";
+  document.getElementById('signup_status').innerHTML = "";
+}
+
+
+function showlogin(){
+  document.getElementById('signup_div').style.display = "none";
+  document.getElementById('login_div').style.display = "none";
+  document.getElementById('wrapper_div').style.display = "flex";
+  document.getElementById('forgot_id').style.display = "none";
+  document.getElementById('login_status').innerHTML = "";
+  document.getElementById('logDIV').innerHTML = "Log-In";
+}
+
+function showforgot(){
+  $("#forgot_id").val('');
+  document.getElementById('forgot_div').style.display = "flex";
+  document.getElementById('signup_div').style.display = "none";
+  document.getElementById('login_div').style.display = "none";
+  document.getElementById('wrapper_div').style.display = "none";
+  document.getElementById('logDIV').innerHTML = "Forgot";
+}
+
+
+function show(){
+  var email = $("#email_field_signup").val();
+  var password = $("#password_field_signup").val();
+
+  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    document.getElementById('signup_status').innerHTML = "<font color='red'>" + errorMessage + "</font>" ;
+    // ...
+  });
+}
+
+
+$("#reset").on("click", function(evt) {
+  evt.preventDefault();
+
+  var emailAddress = $("#forgot_id").val();
+
+  firebase.auth().sendPasswordResetEmail(emailAddress).then(function() {
+    document.getElementById('forgot_status').innerHTML = "<font color='green'> Email Sent! ";
+  }).catch(function(error) {
+    document.getElementById('forgot_status').innerHTML = "<font color='red'> Email not Sent! Try Again!";
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var userName;
+// var farmer_id;
+// var key;
+// var nameData;
+// var idData;
+//
+// $(document).ready(function() {
+// })
+//
+//
+// function push(){
+//   if( username==undefined || farmer_id==undefined ){
+//     alert("Type details!");
+//   }
+//   else{
+//     userName = $("#username").val();
+//     farmer_id = $("#farmerid").val();
+//
+//     // firebase.database().ref('Users').push( userName );
+//     // firebase.database().ref('FarmerID').push( farmer_id );
+//
+//     $("#username").val('');
+//     $("#farmerid").val('');
+//     $("#password").val('');
+//
+//     check();
+//     location.href = "home.html";
+//   }
+// }
+//
+// $("#username").change(function() {
+//   userName = $("#username").val();
+// });
+//
+// $("#farmerid").change(function() {
+//   farmer_id = $("#farmerid").val();
+// });
+//
+//
+// $("#loginButton").on("click", function(evt) {
+//   evt.preventDefault();
+//
+//   push();
+//
+// });
